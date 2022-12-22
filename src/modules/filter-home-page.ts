3fe3941084af883ca8545f0ff89fc6b914756c8f
Alexander_Yulin <ordinaraviro@gmail.com>
2022-12-22 17:08:24 +0200

    feat: add filters container

--- src/modules/filter-home-page.ts
+++ src/modules/filter-home-page.ts
@@ -1,2 +1,25 @@
-import './scss/style.scss';
+import '../scss/style.scss';
 
+function CreateElement(parentElement: HTMLElement, tagName: string, className: string) {
+  const element = document.createElement(tagName);
+  element.classList.add(className);
+  parentElement.append(element);
+}
+
+function CreateFiltersBlock() {
+  CreateElement(document.getElementById('main-page') as HTMLElement, 'div', 'filters-container');
+  CreateElement(document.querySelector('.filters-container') as HTMLElement, 'div', 'filters-block');
+
+  document.querySelector('.cart-block__filters-icon')?.addEventListener('click', () => {
+    document.querySelector('.filters-container')?.classList.toggle('filters-container_show');
+    document.querySelector('.filters-block')?.classList.toggle('filters-block_show');
+  });
+  document.querySelector('.filters-container')?.addEventListener('click', function (this: HTMLElement) {
+    if ((this as HTMLElement) === event?.target) {
+      document.querySelector('.filters-container')?.classList.toggle('filters-container_show');
+      document.querySelector('.filters-block')?.classList.toggle('filters-block_show');
+    }
+  });
+}
+
+export { CreateFiltersBlock };
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,10 @@
 import './index.html';
 import './scss/style.scss';
 import { route, startRouting } from './modules/router/router';
+import { CreateFiltersBlock } from './modules/filter-home-page';
 
 startRouting();
 const menuRow = document.querySelector('#main-nav');
 menuRow?.addEventListener('click', route);
+
+CreateFiltersBlock();
