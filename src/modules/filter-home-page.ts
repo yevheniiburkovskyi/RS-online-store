54ef66326386e057523ec77fb000e5159a760fd2
Alexander_Yulin <ordinaraviro@gmail.com>
2022-12-22 18:09:15 +0200

    feat: add filters buttons

--- src/modules/filter-home-page.ts
+++ src/modules/filter-home-page.ts
@@ -1,14 +1,29 @@
 import '../scss/style.scss';
 
-function CreateElement(parentElement: HTMLElement, tagName: string, className: string) {
+function CreateElement(parentElement: HTMLElement, tagName: string, className: string, content?: string) {
   const element = document.createElement(tagName);
   element.classList.add(className);
+  if (content) element.innerHTML = content;
   parentElement.append(element);
 }
 
 function CreateFiltersBlock() {
   CreateElement(document.getElementById('main-page') as HTMLElement, 'div', 'filters-container');
   CreateElement(document.querySelector('.filters-container') as HTMLElement, 'div', 'filters-block');
+
+  CreateElement(document.querySelector('.filters-block') as HTMLElement, 'div', 'filters-btns-block');
+  CreateElement(
+    document.querySelector('.filters-btns-block') as HTMLElement,
+    'button',
+    'filters-btns-block__reset-btn',
+    'Reset Filters'
+  );
+  CreateElement(
+    document.querySelector('.filters-btns-block') as HTMLElement,
+    'button',
+    'filters-btns-block__copy-link-btn',
+    'Copy Link'
+  );
 
   document.querySelector('.cart-block__filters-icon')?.addEventListener('click', () => {
     document.querySelector('.filters-container')?.classList.toggle('filters-container_show');
