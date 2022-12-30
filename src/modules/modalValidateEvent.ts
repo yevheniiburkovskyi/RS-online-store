b1dacabbd0d11677a3a578143121e5945aacace9
Alexander_Yulin <ordinaraviro@gmail.com>
2022-12-30 18:49:13 +0200

    feat: add card number validation

--- src/modules/modalValidateEvent.ts
+++ src/modules/modalValidateEvent.ts
@@ -27,9 +27,44 @@ inputCvv.addEventListener('input', () => {
 inputCardNum.addEventListener('input', () => {
   inputCardNum.value = inputCardNum.value.replace(/[^\d]/g, '');
   if (inputCardNum.value.length > 4 && inputCardNum.value.length < 9) {
-    inputCardNum.value = inputCardNum.value.replace(/(.{4})(.{4})/g, '$1 $2');
+    inputCardNum.value = inputCardNum.value.replace(/(.{4})(.{1,4})/g, '$1 $2');
   }
-  // inputCardNum.value = inputCardNum.value.replace(/(.{4})/g, '$1 ');
+  if (inputCardNum.value.length > 8 && inputCardNum.value.length < 13 && inputCardNum.value[4] !== ' ') {
+    inputCardNum.value = inputCardNum.value.replace(/(.{4})(.{4})(.{1,4})/g, '$1 $2 $3');
+  }
+  if (inputCardNum.value.length > 12 && inputCardNum.value[4] !== ' ') {
+    inputCardNum.value = inputCardNum.value.replace(/(.{4})(.{4})(.{4})(.{1,4})/g, '$1 $2 $3 $4');
+  }
+
+  document
+    .querySelector('.credit-block__icon')
+    ?.setAttribute(
+      'src',
+      'https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=700&quality=85&auto=format&fit=max&s=fb1dca6cdd4589cd9ef2fc941935de71'
+    );
+  if (inputCardNum.value[0] === '3') {
+    document
+      .querySelector('.credit-block__icon')
+      ?.setAttribute(
+        'src',
+        'https://www.aexp-static.com/cdaas/one/statics/axp-static-assets/1.8.0/package/dist/img/logos/dls-logo-stack.svg'
+      );
+  }
+  if (inputCardNum.value[0] === '4') {
+    document
+      .querySelector('.credit-block__icon')
+      ?.setAttribute('src', 'https://cdn.visa.com/v2/assets/images/logos/visa/blue/logo.png');
+  }
+  if (inputCardNum.value[0] === '5') {
+    document
+      .querySelector('.credit-block__icon')
+      ?.setAttribute('src', 'https://www.mastercard.hu/content/dam/public/mastercardcom/eu/hu/images/mc-logo-52.svg');
+  }
+  if (inputCardNum.value[0] === '6') {
+    document
+      .querySelector('.credit-block__icon')
+      ?.setAttribute('src', 'https://m.unionpayintl.com/imp_file/global/wap/en/static/images/logo.png');
+  }
 });
 
 inputCardDate.addEventListener('input', () => {
