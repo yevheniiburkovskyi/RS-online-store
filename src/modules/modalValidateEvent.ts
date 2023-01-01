7a7e6d17dc215d7b6cf659c04a478494bf8de9ec
Alexander_Yulin <ordinaraviro@gmail.com>
2023-01-01 13:15:50 +0200

    feat: add card icons from assets

--- src/modules/modalValidateEvent.ts
+++ src/modules/modalValidateEvent.ts
@@ -38,34 +38,19 @@ inputCardNum.addEventListener('input', () => {
     inputCardNum.value = inputCardNum.value.replace(/(.{4})(.{4})(.{4})(.{1,4})/g, '$1 $2 $3 $4');
   }
 
-  document
-    .querySelector('.credit-block__icon')
-    ?.setAttribute(
-      'src',
-      'https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=700&quality=85&auto=format&fit=max&s=fb1dca6cdd4589cd9ef2fc941935de71'
-    );
+  (document.querySelector('.credit-block__icon') as HTMLElement).className = 'credit-block__icon';
+
   if (inputCardNum.value[0] === '3') {
-    document
-      .querySelector('.credit-block__icon')
-      ?.setAttribute(
-        'src',
-        'https://www.aexp-static.com/cdaas/one/statics/axp-static-assets/1.8.0/package/dist/img/logos/dls-logo-stack.svg'
-      );
+    (document.querySelector('.credit-block__icon') as HTMLElement).classList.add('credit-block__icon_aexpress');
   }
   if (inputCardNum.value[0] === '4') {
-    document
-      .querySelector('.credit-block__icon')
-      ?.setAttribute('src', 'https://cdn.visa.com/v2/assets/images/logos/visa/blue/logo.png');
+    (document.querySelector('.credit-block__icon') as HTMLElement).classList.add('credit-block__icon_visa');
   }
   if (inputCardNum.value[0] === '5') {
-    document
-      .querySelector('.credit-block__icon')
-      ?.setAttribute('src', 'https://www.mastercard.hu/content/dam/public/mastercardcom/eu/hu/images/mc-logo-52.svg');
+    (document.querySelector('.credit-block__icon') as HTMLElement).classList.add('credit-block__icon_mcard');
   }
   if (inputCardNum.value[0] === '6') {
-    document
-      .querySelector('.credit-block__icon')
-      ?.setAttribute('src', 'https://m.unionpayintl.com/imp_file/global/wap/en/static/images/logo.png');
+    (document.querySelector('.credit-block__icon') as HTMLElement).classList.add('credit-block__icon_up');
   }
 });
 
