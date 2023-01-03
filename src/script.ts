eaa2761cce50c6f36bd43fecc34733129bdd1c84
Eugene <gradam2001@gmail.com>
2023-01-03 17:11:44 +0200

    feat: add scrollUp

--- src/script.ts
+++ src/script.ts
@@ -5,11 +5,13 @@ import { startRouting } from './modules/router/router';
 import mainPage from './pages/mainPage/mainPage';
 import imgShow from './modules//imgShow';
 import getData from './modules/services/getData';
+import scrollUp from './modules/scroll';
 
 window.addEventListener('DOMContentLoaded', () => {
   getData().then((data) => {
     startRouting();
     mainPage(data);
     imgShow();
+    scrollUp();
   });
 });
